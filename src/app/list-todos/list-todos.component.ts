b1b5bb8c84a2bb43262f546967cadb1cc56b9860
4m4r199707 <34017886+4m4r199707@users.noreply.github.com>
2020-04-10 00:00:04 +0530

    Hardcoded getall todos and delete feature.

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,19 +1,40 @@
 import { Component, OnInit } from "@angular/core";
+import { TodoDataService } from "../service/data/todo-data.service";
 
 @Component({
   selector: "app-list-todos",
   templateUrl: "./list-todos.component.html",
-  styleUrls: ["./list-todos.component.css"]
+  styleUrls: ["./list-todos.component.css"],
 })
 export class ListTodosComponent implements OnInit {
-  todos = [
-    new Todo(1, "Amar", new Date(), true),
-    new Todo(1, "Amrutha", new Date(), false),
-    new Todo(1, "Kamala Kumar", new Date(), true)
-  ];
-  constructor() {}
+  todos: Todo[];
 
-  ngOnInit() {}
+  message: string;
+  //  = [
+  //   new Todo(1, "Amar", new Date(), true),
+  //   new Todo(1, "Amrutha", new Date(), false),
+  //   new Todo(1, "Kamala Kumar", new Date(), true)
+  // ];
+  constructor(private service: TodoDataService) {}
+
+  ngOnInit() {
+    this.refreshTodos();
+  }
+
+  refreshTodos() {
+    this.service.retriveAllTodos("amar").subscribe((res) => {
+      this.todos = res;
+      console.log(res);
+    });
+  }
+  deleteTodo(id) {
+    // console.log(`delete todo ${id}`);
+    this.service.deleteTodo("amar", id).subscribe((res) => {
+      console.log(res);
+      this.message = `delete of Todo ${id}succesfull!! `;
+      this.refreshTodos();
+    });
+  }
 }
 
 export class Todo {
